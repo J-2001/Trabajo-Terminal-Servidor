84cfab859a32ca78ff2991a17e49bbc6f15fa04f
J-2001 <roldangomezjuan01@gmail.com>
2024-04-03 01:31:07 -0600

    Google Cloud Configuration

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const { initializeApp } = require('firebase-admin/app')
-const { createServer } = require('node:http');
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello from App Engine!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+
+/* Firebase
+const { initializeApp } = require('firebase-admin/app')
 const app = initializeApp()
+*/
+
+/* Example Application
+const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -15,3 +30,4 @@ const server = createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
