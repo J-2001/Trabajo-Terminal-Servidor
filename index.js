e37f056fff119e97b74d6a138c5241e6cf3ca93f
J-2001 <roldangomezjuan01@gmail.com>
2024-04-03 18:22:59 -0600

    Google Cloud Configuration

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/* Firebase Admin SDK */
+const { initializeApp } = require('firebase-admin/app');
+const app = initializeApp();
+
 const express = require('express');
 const app = express();
 
@@ -9,11 +13,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
-
-/* Firebase
-const { initializeApp } = require('firebase-admin/app')
-const app = initializeApp()
-*/
 
 /* Example Application
 const { createServer } = require('node:http');
